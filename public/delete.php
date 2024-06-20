ddc40a15055391f9d6cc3f21eb5355cd9d4100f1
Elly-Joy <ellyjoy.waheho@strathmore.edu>
2024-06-20 21:32:50 +0300

    index.html

--- public/delete.php
+++ public/delete.php
@@ -1,7 +1,4 @@
-:<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    $id = $data['id'];
+
 
     // Database connection details
     $conn = mysqli_connect("localhost", "root", "", "veripay");
